0767d91f916ab9ad287f85957d78ed4c718a5cc9
Zamanhuseyinli <zamanhuseynli23@gmail.com>
2026-08-08 01:33:17 +0400

    Updated: Added version codes ABI.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -76,22 +76,25 @@ android {
         }
     }
 }
-applicationVariants.all {
-    outputs.forEach { output ->
-        val abiCodes = mapOf(
-            "armeabi-v7a" to 0,
-            "arm64-v8a" to 1,
-            "x86" to 2,
-            "x86_64" to 3
-        )
+androidComponents {
+    onVariants { variant ->
+        variant.outputs.forEach { output ->
+            val abi = output.filters
+                .find { it.filterType.name == "ABI" }
+                ?.identifier
 
-        val abi = output.filters
-            .find { it.filterType == "ABI" }
-            ?.identifier
+            val abiCodes = mapOf(
+                "armeabi-v7a" to 1,
+                "arm64-v8a" to 2,
+                "x86" to 3,
+                "x86_64" to 4
+            )
 
-        if (abi != null) {
-            output.versionCodeOverride =
-                defaultConfig.versionCode * 10 + abiCodes[abi]!!
+            if (abi != null) {
+                output.versionCode.set(
+                    (variant.versionCode.orNull ?: 0) * 10 + (abiCodes[abi] ?: 0)
+                )
+            }
         }
     }
 }
