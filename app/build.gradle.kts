9ada876a8c9425ea4862da3b84fd4966e10a31ba
Zamanhuseyinli <zamanhuseynli23@gmail.com>
2026-08-26 04:36:30 +0400

    Bump version 0.9.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "com.terminator.app"
         minSdk = 33
         targetSdk = 37
-        versionCode = 14
-        versionName = "0.8.1"
+        versionCode = 15
+        versionName = "0.9.1"
     }
     buildFeatures {
         compose = true
